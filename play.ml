55c331dd6d9165cf4efacb715e37858d2b4b3fbc
Yaron Minsky <yminsky@janestreet.com>
2016-09-17 16:47:01 -0400

    added util

--- play.ml
+++ play.ml
@@ -1,24 +1,5 @@
 open! Import
-
-let printf = Printf.printf
-let st = String.strip
-
-let sayf format =
-  Printf.ksprintf print_endline format
-
-let prompt () =
-  printf "\n> %!";
-  match input_line stdin with
-  | exception _ -> Other []
-  | x ->
-    let words = 
-      let open List.Let_syntax in
-      String.split x ~on:' '
-      >>| String.strip
-      >>| String.lowercase
-      >>| String.filter ~f:Char.is_alphanum
-    in
-    Parser.run words
+open Util
 
 (** ROOMS **)
 
