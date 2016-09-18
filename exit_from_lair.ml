7ee6e2a4b3957e30b76ec4c3f1ad7de2c09f91c8
Yaron Minsky <yminsky@janestreet.com>
2016-09-17 22:40:50 -0400

    language

--- exit_from_lair.ml
+++ exit_from_lair.ml
@@ -3,9 +3,9 @@ open Import
 let here = Room.Exit_from_lair
 let things = []
 let desc _ = {|
-The corridor out of the lair angles up, and soon enough you can feel a
-change to the taste of the air. Moments later you see light, and then,
-finally, you're above ground.
+The corridor out of the lair angles up, and soon enough you can taste
+the change in the air. Moments later you see light, and then, finally,
+you're above ground.
 
 That was an oddly disturbing experience, but you can't help hoping for
 a future time when your father has had the opportunity to write some
